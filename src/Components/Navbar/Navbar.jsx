4081625b23c226397fe5ace37677670c2c03a6d8
Ananthu M <ananthu.m.utr@gmail.com>
2025-07-16 11:39:23 +0530

    navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import './Navbar.css'
+import { useState } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import './Navbar.css';
+
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-      <div className="navbar">
-        <div className="name">
-          ANANTHU M
-        </div>
-        <ul className='menu-bar'>
-          <li><AnchorLink className='anchor-link' href="#home" >Home</AnchorLink></li>
-          <li><AnchorLink className='anchor-link' offset={50} href="#about" >About Me</AnchorLink></li>
-          <li><AnchorLink className='anchor-link' offset={50} href="#projects" >Projects</AnchorLink></li>
-          <li><AnchorLink className='anchor-link' offset={50} href="#skills" >Skills</AnchorLink></li>
-          <li><AnchorLink className='anchor-link' offset={50} href="#education">Education</AnchorLink></li>
-        </ul>
-        <div className="contact"><AnchorLink className='anchor-link' offset={50} href="#contact">Contact</AnchorLink></div>
+    <div className="navbar">
+      <div className="name">ANANTHU M</div>
+
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776;
       </div>
-  )
+
+      {/* Desktop Menu */}
+      <ul className={`menu-bar ${isMobileMenuOpen ? 'active' : ''}`}>
+        <li><AnchorLink className='anchor-link' href="#home">Home</AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href="#about">About Me</AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href="#projects">Projects</AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href="#skills">Skills</AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href="#education">Education</AnchorLink></li>
+        <li className='contact-link'><AnchorLink className='anchor-link' offset={50} href="#contact">Contact</AnchorLink></li>
+      </ul>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
